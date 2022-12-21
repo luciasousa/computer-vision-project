fc06d643e5a154b31b9bc31761bd5251b84f4326
luciasousa <luciamsousa00@ua.pt>
2022-12-21 18:16:03 +0000

    questions solved

--- webcam/photo_test.py
+++ webcam/photo_test.py
@@ -6,75 +6,11 @@ import sys
 from matplotlib import pyplot as plt
 import numpy as np
 
-'''
-def init_matrix(matrix):
-    #first row of matrix is None,a,b,c,d,None,a,b,c,d,None,a,b,c,d
-    matrix[0][0] = None
-    matrix[0][1] = 'a'
-    matrix[0][2] = 'b'
-    matrix[0][3] = 'c'
-    matrix[0][4] = 'd'
-    matrix[0][5] = None
-    matrix[0][6] = 'a'
-    matrix[0][7] = 'b'
-    matrix[0][8] = 'c'
-    matrix[0][9] = 'd'
-    matrix[0][10] = None
-    matrix[0][11] = 'a'
-    matrix[0][12] = 'b'
-    matrix[0][13] = 'c'
-    matrix[0][14] = 'd'
-
-    #first column of matrix is None,1,2,3,4,5,6,7,8,9,10,11,12,13,14
-    matrix[1][0] = 1
-    matrix[2][0] = 2
-    matrix[3][0] = 3
-    matrix[4][0] = 4
-    matrix[5][0] = 5
-    matrix[6][0] = 6
-    matrix[7][0] = 7
-    matrix[8][0] = 8
-    matrix[9][0] = 9
-    matrix[10][0] = 10
-    matrix[11][0] = 11
-    matrix[12][0] = 12
-    matrix[13][0] = 13
-    matrix[14][0] = 14
-
-    #sixt column of matrix is None,15,16,17,18,19,20,21,22,23,24,25,26,27,28
-    matrix[1][5] = 15
-    matrix[2][5] = 16
-    matrix[3][5] = 17
-    matrix[4][5] = 18
-    matrix[5][5] = 19
-    matrix[6][5] = 20
-    matrix[7][5] = 21
-    matrix[8][5] = 22
-    matrix[9][5] = 23
-    matrix[10][5] = 24
-    matrix[11][5] = 25
-    matrix[12][5] = 26
-    matrix[13][5] = 27
-    matrix[14][5] = 28
-
-    #eleven column of matrix is None,29,30,31,32,33,34,35,36,37,38,39,40,41,42
-    matrix[1][10] = 29
-    matrix[2][10] = 30
-    matrix[3][10] = 31
-    matrix[4][10] = 32
-    matrix[5][10] = 33
-    matrix[6][10] = 34
-    matrix[7][10] = 35
-    matrix[8][10] = 36
-    matrix[9][10] = 37
-    matrix[10][10] = 38
-    matrix[11][10] = 39
-    matrix[12][10] = 40
-    matrix[13][10] = 41
-    matrix[14][10] = 42
-
-    return matrix
-'''
+NUMBER_OF_QUESTIONS = 38
+NUMBER_OF_QUESTIONS_VF = 12
+NUMBER_OF_LINES = 14
+NUMBER_OF_COLUMNS = 3
+NUMBER_OF_COLUMNS_VF = 1
 
 def map_coordinates_x(coordinates_rectangles):
 
@@ -139,13 +75,15 @@ while True:
     frame_markers = cv2.aruco.drawDetectedMarkers(frame.copy(), corners, ids)
     if ids is not None:
         length = len(ids)
-        if length == 4:
+        if length == 4: #meter 4 se nao tiver perguntas v/F
             for i in range(len(corners)):
+                print(ids[i])
                 c = corners[i][0]# c type = numpy.ndarray
-                #cv2.line(frame, tuple(c[0].astype('int32')), tuple(c[1].astype('int32')), (255,0,255), 5)
-                #cv2.line(frame, tuple(c[1].astype('int32')), tuple(c[2].astype('int32')), (255,0,255), 5)
-                #cv2.line(frame, tuple(c[2].astype('int32')), tuple(c[3].astype('int32')), (255,0,255), 5)
-                #cv2.line(frame, tuple(c[3].astype('int32')), tuple(c[0].astype('int32')), (255,0,255), 5)
+                cv2.line(frame, tuple(c[0].astype('int32')), tuple(c[1].astype('int32')), (255,0,255), 5)
+                cv2.line(frame, tuple(c[1].astype('int32')), tuple(c[2].astype('int32')), (255,0,255), 5)
+                cv2.line(frame, tuple(c[2].astype('int32')), tuple(c[3].astype('int32')), (255,0,255), 5)
+                cv2.line(frame, tuple(c[3].astype('int32')), tuple(c[0].astype('int32')), (255,0,255), 5)
+                #print(c)
                 if ids[i] == 1:
                     cv2.circle(frame, tuple(c[3].astype('int32')), 5, (255,0,0), -1)
                     x1,y1 = c[3].astype('int32')
@@ -155,15 +93,39 @@ while True:
                 if ids[i] == 3:
                     cv2.circle(frame, tuple(c[0].astype('int32')), 5, (255,0,0), -1)
                     x3,y3 = c[0].astype('int32')
-                if ids[i] == 5:
+                '''
+                if ids[i] == 4: 
                     cv2.circle(frame, tuple(c[1].astype('int32')), 5, (255,0,0), -1)
                     x4,y4 = c[1].astype('int32')
+                '''
+                if ids[i] == 5:
+                    cv2.circle(frame, tuple(c[1].astype('int32')), 5, (255,0,0), -1)
+                    x5,y5 = c[1].astype('int32')
+                '''
+                if ids[i] == 6:
+                    cv2.circle(frame, tuple(c[2].astype('int32')), 5, (255,0,0), -1)
+                    x6,y6 = c[2].astype('int32')
+                '''
+                #V/F
+            '''
+            cv2.line(frame, (x1,y1), (x6,y6), (0,255,0), 5)
+            cv2.line(frame, (x6,y6), (x4,y4), (0,255,0), 5)
+            cv2.line(frame, (x3,y3), (x4,y4), (0,255,0), 5)
+            cv2.line(frame, (x3,y3), (x1,y1), (0,255,0), 5)
+                
+            pts1 = np.float32([[x1,y1],[x2,y2],[x3,y3],[x4,y4],[x5,y5],[x6,y6]])
+            pts2 = np.float32([[0,0],[500,0],[0,500],[500,500], [0,500], [500,500]])
+            M = cv2.getPerspectiveTransform(pts1,pts2)
+            dst = cv2.warpPerspective(frame,M,(500,500))
+            '''
+
             cv2.line(frame, (x1,y1), (x2,y2), (0,255,0), 5)
-            cv2.line(frame, (x2,y2), (x4,y4), (0,255,0), 5)
-            cv2.line(frame, (x4,y4), (x3,y3), (0,255,0), 5)
+            cv2.line(frame, (x2,y2), (x5,y5), (0,255,0), 5)
+            cv2.line(frame, (x5,y5), (x3,y3), (0,255,0), 5)
             cv2.line(frame, (x3,y3), (x1,y1), (0,255,0), 5)
 
-            pts1 = np.float32([[x1,y1],[x2,y2],[x3,y3],[x4,y4]])
+            #perspective correction
+            pts1 = np.float32([[x1,y1],[x2,y2],[x3,y3],[x5,y5]])
             pts2 = np.float32([[0,0],[500,0],[0,500],[500,500]])
             M = cv2.getPerspectiveTransform(pts1,pts2)
             dst = cv2.warpPerspective(frame,M,(500,500))
@@ -180,7 +142,6 @@ while True:
 
 cam.release()
 cv2.destroyAllWindows()
-
 
 
 dst_final = cv2.imread('photo_test_image.jpg')
@@ -224,7 +185,7 @@ percentage_wht=0
 list_x = [] #list with x coordinates
 list_y = [] #list with y coordinates
 #matrix with 15 rows and 15 columns
-matrix = [[0 for x in range(15)] for y in range(15)]
+matrix_questions = [[0 for x in range(NUMBER_OF_LINES+1)] for y in range(NUMBER_OF_LINES+1)]
 
 for x,y,w,h,area in stats[2:]:
     ss_count += 1
@@ -259,8 +220,8 @@ new_y , y_velho= map_coordinates_y(coordinates_rectangles)
 
 #print(coordinates_rectangles)
 
-print(new_x)
-print(new_y)
+#print(new_x)
+#print(new_y)
 
 x_novo = 0
 y_novo = 0
@@ -284,145 +245,98 @@ for i in coordinates_rectangles:
     if percentage_blk > 15 and percentage_wht > 30: #has an x
         cv2.circle(dst_final, (x, y), 5, (255,0,0), -1)
         #print("x, y: ", x_novo, y_novo)
-        matrix[y_novo][x_novo] = 1
+        matrix_questions[y_novo][x_novo] = 1
     if percentage_blk > 80: 
         cv2.circle(dst_final, (x, y), 5, (0,0,255), -1) 
 
 #init_matrix(matrix)
+'''
 for row in matrix:
     print(row)
-    
+'''
 
 
 cv2.imwrite("photo_final_dst.jpg", dst_final)
 
 
 #remove first line from matrix
-matrix.pop(0)
+matrix_questions.pop(0)
+
 #remove first column from matrix
-for row in matrix:
-    row.pop(0)
-    row.pop(4)
-    row.pop(8)
-
-
+for row in matrix_questions:
+    r = 0
+    row.pop(r)
+    for i in range(NUMBER_OF_COLUMNS-1):
+        r +=4
+        row.pop(r)
+'''
 print("matrix pop")
 for row in matrix:
     print(row)
+'''
+
 
 #create new matrix with 14 rows and 12 columns
-matrix_14_12 = [[0 for x in range(12)] for y in range(14)]
+matrix_questions_final = [[0 for x in range(NUMBER_OF_COLUMNS*4)] for y in range(NUMBER_OF_LINES)]
 
 #copy values from matrix to matrix_14_12
 for i in range(14):
     for j in range(12):
-        matrix_14_12[i][j] = matrix[i][j]
+        matrix_questions_final[i][j] = matrix_questions[i][j]
 
+'''
 print("matrix_14_12 -----------------------------")
 for row in matrix_14_12:
     print(row)
+'''
 
 col_min = 0
 col_max = 4
 
 count_res = 0
 
-array_answers = [0 for x in range(42)]
+array_answers = [0 for x in range(NUMBER_OF_QUESTIONS)]
+array_answers_vf = [0 for x in range(NUMBER_OF_QUESTIONS_VF)]
+i_aux = 0
+for i in range(NUMBER_OF_COLUMNS):
+    for i in range(NUMBER_OF_LINES):
+        count_res = 0
+        for j in range(col_min, col_max):
+            if matrix_questions_final[i][j] == 1:
+                count_res = 1
+                if j%4==0:
+                    array_answers[i_aux] = 'a'
+                if (j-1)%4==0:
+                    array_answers[i_aux] = 'b'
+                if (j-2)%4==0:
+                    array_answers[i_aux] = 'c'
+                if (j-3)%4==0:
+                    array_answers[i_aux] = 'd'
+            elif count_res == 0:
+                array_answers[i_aux] = '-'
+        i_aux += 1
+        if i_aux == NUMBER_OF_QUESTIONS:
+            break
+    col_min += 4
+    col_max += 4
 
-for i in range(14):
-    count_res = 0
-    for j in range(col_min, col_max):
-        if matrix_14_12[i][j] == 1:
-            count_res = 1
-            if j == 0:
-                array_answers[i] = 'a'
-            if j == 1: 
-                array_answers[i] = 'b'
-            if j == 2:
-                array_answers[i] = 'c'
-            if j == 3:
-                array_answers[i] = 'd'
-        elif count_res == 0:
-            array_answers[i] = '-'
+'''
+for i in range(NUMBER_OF_COLUMNS_VF):
+    for i in range(NUMBER_OF_LINES):
+        count_res = 0
+        for j in range(col_min, col_max):
+            if matrix_14_12[i][j] == 1:
+                count_res = 1
+                if j
+                
+            elif count_res == 0:
+                array_answers[i_aux] = '-'
+        i_aux += 1
+        if i_aux == NUMBER_OF_QUESTIONS:
+            break
+    col_min += 2
+    col_max += 2
+'''
 
-col_min += 4
-col_max += 4
-
-for i in range(14):
-    i_aux = i
-    count_res = 0
-    i_aux += 14
-    for j in range(col_min, col_max):
-        if matrix_14_12[i][j] == 1:
-            count_res = 1
-            if j == 4:
-                array_answers[i_aux] = 'a'
-            if j == 5: 
-                array_answers[i_aux] = 'b'
-            if j == 6:
-                array_answers[i_aux] = 'c'
-            if j == 7:
-                array_answers[i_aux] = 'd'
-        elif count_res == 0:
-            array_answers[i_aux] = '-'
-
-col_min += 4
-col_max += 4
-
-for i in range(14):
-    i_aux = i
-    count_res = 0
-    i_aux += 28
-    for j in range(col_min, col_max):
-        if matrix_14_12[i][j] == 1:
-            count_res = 1
-            if j == 8:
-                array_answers[i_aux] = 'a'
-            if j == 9: 
-                array_answers[i_aux] = 'b'
-            if j == 10:
-                array_answers[i_aux] = 'c'
-            if j == 11:
-                array_answers[i_aux] = 'd'
-        elif count_res == 0:
-            array_answers[i_aux] = '-'
 
 print(array_answers)
-'''
-#create matrix with 42 rows and 4 columns
-matrix_42_4 = [[0 for x in range(4)] for y in range(42)]
-
-col_min = 0
-col_max = 3
-
-range_i = 14
-
-lines = 0
-lines_aux = 0
-
-for j in range(col_min, col_max):
-    for i in range(14):
-        matrix_42_4[lines][j//3] = matrix_14_12[i][j]
-        lines = i + lines_aux
-    
-    lines_aux += 14
-    col_min = col_min + 4
-    col_max = col_max + 4
-    
-
-#copy values from matrix_14_12 to matrix_42_4
-for i in range(14):
-    for j in range(col_min, col_max):
-        matrix_42_4[lines][j//3] =0# matrix_14_12[i][j]
-        lines += 1
-
-    
-    col_min = col_min + 4
-    col_max = col_max + 4
-    print ("lines, cols: ", lines, col_min, col_max)
-
-
-print("matrix_42_4 -----------------------------")
-for row in matrix_42_4:
-    print(row)
-'''
